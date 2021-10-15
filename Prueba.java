65b5812df9533e7db2b325958f1667dfef298ec9
Javier <alvmurjav@gmail.com>
2021-10-15 09:37:29 +0200

    Cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -1,9 +1,10 @@
 public class Prueba{
 	public static void main (String args[]){
 	
-	System.out.println("Nombre: Javier Alvarez Murciego");
+	//System.out.println("Nombre: Javier Alvarez Murciego");
 	System.out.println("Nacimiento: Leon Ano: 2002");
 	System.out.println("Sistema Operativo: Windows");
+	System.out.println("Cantante favorito: Estopa");
 	}
 
 }
